4400cccfb91dff24403efe1d3c288508d9fee5d3
Hiroki KIYOHARA <hirokiky@gmail.com>
2017-08-19 14:34:14 +0900

    Added default_app_config and not to import __init__
    Importing on keeper's __init__ causes runtime error.
    because it will use settings before django initialization completed.

--- keeper/__init__.py
+++ keeper/__init__.py
@@ -1,2 +1,1 @@
-from keeper.security import Allow, Deny, Everyone, Authenticated, Staff, has_permission  # NOQA
-from keeper.views import keeper  # NOQA
+default_app_config = 'keeper.apps.KeeperConfig'
