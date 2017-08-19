7f003269dfbe37c7af803a489cbd8f0759907d89
Hiroki KIYOHARA <hirokiky@gmail.com>
2017-08-19 22:51:53 +0900

    Added url names for demo app

--- demo/magazines/core/urls.py
+++ demo/magazines/core/urls.py
@@ -4,11 +4,12 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^team/manage/$', views.team_manage),
-    url(r'^team/$', views.team_dashboard),
-    url(r'^account/billing/$', views.team_billing),
-    url(r'^account/billing/resign/$', views.team_billing_resign),
-    url(r'^$', views.dashboard),
-    url(r'^magazines/(?P<magazine_id>\d+)/$', views.magazine_detail),
-    url(r'^magazines/(?P<magazine_id>\d+)/(?P<article_id>\d+)/$', views.article_detail),
+    url(r'^team/manage/$', views.team_manage, name='team_manage'),
+    url(r'^team/$', views.team_dashboard, name='team_dashboard'),
+    url(r'^team/billing/$', views.team_billing, name='team_billing'),
+    url(r'^team/billing/resign/$', views.team_billing_resign, name='team_billing_resign'),
+    url(r'^$', views.dashboard, name='dashboard'),
+    url(r'^magazines/(?P<magazine_id>\d+)/$', views.magazine_detail, name='magazine_detail'),
+    url(r'^magazines/(?P<magazine_id>\d+)/(?P<article_id>\d+)/$', views.article_detail,
+        name='article_detail'),
 ]
