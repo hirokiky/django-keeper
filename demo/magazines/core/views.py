2afa47d3ceab8ff7ce93c2d32af1423611d1539c
Hiroki KIYOHARA <hirokiky@gmail.com>
2017-08-19 15:21:43 +0900

    Fixed to use on_fail on demo app

--- demo/magazines/core/views.py
+++ demo/magazines/core/views.py
@@ -1,4 +1,4 @@
-from keeper.views import keeper
+from keeper.views import keeper, login_required
 
 from core.models import Magazine, Article
 
@@ -23,16 +23,21 @@ def team_billing_resign(request):
     pass
 
 
-@keeper('list_magazines')
+@keeper('list_magazines',
+        on_fail=login_required())
 def dashboard(request):
     pass
 
 
-@keeper('read', Magazine, lambda r, mid: {'id': mid})
+@keeper('read',
+        model=Magazine,
+        mapper=lambda request, magazine_id: {'id': magazine_id})
 def magazine_detail(request, magazine_id):
     pass
 
 
-@keeper('read', Article, lambda r, aid: {'id': aid})
-def article_detail(request, article_id):
+@keeper('read',
+        model=Article,
+        mapper=lambda request, magazine_id, article_id: {'id': article_id})
+def article_detail(request, magazine_id, article_id):
     pass
