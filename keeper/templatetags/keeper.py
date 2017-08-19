d642898f65e312639c162f21861b1290dd08fb5d
Hiroki KIYOHARA <hirokiky@gmail.com>
2017-08-19 22:52:05 +0900

    Fixed to handle None on template tag

--- keeper/templatetags/keeper.py
+++ keeper/templatetags/keeper.py
@@ -7,5 +7,8 @@ register = Library()
 
 @register.simple_tag(takes_context=True)
 def has_permission(context, obj, permission):
+    if obj is None:
+        return False
+
     request = context['request']
     return security.has_permission(permission, obj, request)
