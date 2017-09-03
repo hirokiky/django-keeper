92146acdb5dc60f9b4d852f209683613e3aa8ea0
Hiroki KIYOHARA <hirokiky@gmail.com>
2017-09-03 12:01:47 +0900

    Added tests

--- tests/test_security.py
+++ tests/test_security.py
@@ -5,10 +5,12 @@ from django.test import TestCase, RequestFactory
 from keeper.security import (
     detect_permissions,
     Allow,
+    Deny,
 )
 from keeper.operators import (
     Everyone,
     Authenticated,
+    IsUser,
 )
 
 
@@ -17,26 +19,68 @@ dummy_user = Mock(
     is_staff=False,
 )
 
-dummy_staff = Mock(
-    is_authenticated=True,
-    is_staff=True,
-)
-
-
 rf = RequestFactory()
 
 
 class Model:
+    def __init__(self, acl_function):
+        self.acl_function = acl_function
+
     def __acl__(self):
-        return [
-            (Allow, Everyone, 'view'),
-            (Allow, Authenticated, 'edit'),
-        ]
+        return self.acl_function()
 
 
 class TestDetectPermissions(TestCase):
     def test_it(self):
+        context = Model(lambda: [
+            (Allow, Everyone, 'view'),
+            (Allow, Authenticated, 'edit'),
+        ])
         req = rf.get('/')
         req.user = dummy_user
-        actual = detect_permissions(Model(), req)
+        actual = detect_permissions(context, req)
         self.assertEqual(actual, {'view', 'edit'})
+
+    def test_no_acl(self):
+        req = rf.get('/')
+        with self.assertRaises(TypeError):
+            detect_permissions(object(), req)
+
+    def test_multiple_permissions(self):
+        context = Model(lambda: [
+            (Allow, Everyone, ('view', 'edit')),
+        ])
+        req = rf.get('/')
+        actual = detect_permissions(context, req)
+        self.assertEqual(actual, {'view', 'edit'})
+
+    def test_operator_object(self):
+        context = Model(lambda: [
+            (Allow, IsUser(dummy_user), 'view'),
+        ])
+        req = rf.get('/')
+        req.user = dummy_user
+        actual = detect_permissions(context, req)
+        self.assertEqual(actual, {'view'})
+
+    def test_deny(self):
+        context = Model(lambda: [
+            (Allow, Everyone, ('view', 'edit')),
+            (Deny, IsUser(dummy_user), 'edit'),
+        ])
+        req = rf.get('/')
+        req.user = dummy_user
+        actual = detect_permissions(context, req)
+        self.assertEqual(actual, {'view'})
+
+        req = rf.get('/')
+        actual = detect_permissions(context, req)
+        self.assertEqual(actual, {'view', 'edit'})
+
+    def test_not_matched(self):
+        context = Model(lambda: [
+            (Allow, Authenticated, 'view'),
+        ])
+        req = rf.get('/')
+        actual = detect_permissions(context, req)
+        self.assertEqual(actual, set())
