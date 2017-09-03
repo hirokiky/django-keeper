0bd035afc7a7226d9fd0bdec87b135099e42c1d0
Hiroki KIYOHARA <hirokiky@gmail.com>
2017-09-03 16:17:41 +0900

    Improved packaging

--- setup.py
+++ setup.py
@@ -1,10 +1,22 @@
+import os
 from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
 
 
 setup(
     name='django-keeper',
-    version='0.1.1',
+    version='0.1.2',
     install_requires=[
         'Django>=1.10,<2.0',
-    ]
+    ],
+    author='Hiroki KIYOHARA',
+    author_email='hirokiky@gmail.com',
+    url='https://github.com/hirokiky/django-keeper/',
+    license='MIT',
+    description="Authorization library for Django, with ACL, not depends on models.",
+    long_description=README,
+    test_suite="tests",
+    packages=['keeper'],
 )
